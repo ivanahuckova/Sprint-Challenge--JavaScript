779fa916be68df3830224ca4fffe547c7acea2bf
Ivana Huckova <ivana.huckova@gmail.com>
2019-01-25 10:20:57 +0100

    Complete functions.js challenges

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,9 +1,5 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
-
 // Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
 
 /* == Step 1: Base Constructor ==
@@ -43,6 +39,25 @@ class CuboidMaker2 {
 	}
 }
 
-const cuboid1 = new CuboidMaker2(4, 5, 5);
+const cuboid2 = new CuboidMaker2(4, 5, 5);
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid2.volume()); // 100
+console.log(cuboid2.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+// class CubeMaker extends CuboidMaker2 {
+// 	constructor() {}
+// 	volumeCube() {
+// 		return length * length * length;
+// 	}
+// 	surfaceAreaCube() {
+// 		return 6 * length * length;
+// 	}
+// }
+
+// const cube = new CubeMaker(5, 5, 5);
+
+// console.log(cube.volumeCube());
+// console.log(cube.surfaceAreaCube());
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
  * The last parameter accepts a callback
  * In the body of the function return the callback with the two parameters that you created
  */
+function consume(param1, param2, cb) {
+	cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
@@ -13,16 +16,33 @@
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
 
+function add(a, b) {
+	return a + b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function greeting(first_name, last_name) {
+	return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
+
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2, 2, add); // 4
+consume(10, 16, multiply); // 160
+consume("Mary", "Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation:
+/* Explanation: 
+`nestedfunction()` is a closure. Closure is a feature in JavaScript where an inner function has access to the outer function’s variables. Closure has three scope chains:
+1. It has access to its own scope — variables defined between its curly brackets
+2. It has access to the outer function’s variables
+3. It has access to the global variables
+Therefore `nestedfunction()` can access variable internal, as it is the outer function’s variables*/
 
 const external = "I'm outside the function";
 
